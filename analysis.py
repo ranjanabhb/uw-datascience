7ba3ec45db58517d34595cdeab2aa016ad880e41
Ranjanabh Bahukhandi <ranjanabhb@gmail.com>
2015-12-13 23:09:20 -0500

    Code Cleanup

--- analysis.py
+++ analysis.py
@@ -14,13 +14,11 @@ def extractLatLng(location):
     (lat, lng) = location.split(",")
     return (lat[1:8], lng[:10])
 
-def printCounter(counter, topN = None):
-    print 
+def printCounter(counter, topN = None): 
     for (key, value) in counter.most_common(topN):
         print (str(key) + ", " + str(value))    
 
 def printSortedList(lst):
-    print 
     for item in lst:
         print (str(item[0]) + ", " + str(item[1]))            
 
@@ -45,33 +43,39 @@ def processDF(df):
     zipCode = Counter()
     print ("\nDF Len - " + str(len(df)))    
     extractColumnFromDf(df, 'Category', category)
+    print ("\nTop 10 Categories:")
     printCounter(category, 10)
 
     extractColumnFromDf(df, 'DayOfWeek', dayOfWeek)
+    print ("\nIncidents per DayOfWeek: ")
     printCounter(dayOfWeek, 7)
 
     extractColumnFromDf(df, 'Date', month, extractMonth)
     sortedMonth = sorted(month.items())
+    print ("\nIncidents per Month: ")
     printSortedList(sortedMonth)
 
     extractColumnFromDf(df, 'Time', hourOfTheDay, extractHour)
     sortedHourOfTheDay = sorted(hourOfTheDay.items())
+    print ("\nIncidents per Hour: ")
     printSortedList(sortedHourOfTheDay)
 
     extractColumnFromDf(df, 'Location', latLng, extractLatLng)
     extractZipCodeFromLatLng(latLng, zipCode) 
+    print ("\nIncidents per top 5 ZipCodes: ")
     printCounter(zipCode)
 
-    zcList = [(key, value) for (key, value) in zipCode.most_common(4)]
+    zcList = [(key, value) for (key, value) in zipCode.most_common(5)]
     plotBar(zcList, 'Incidents Per ZipCode', 'Incident Count', 5)
     
 def main():
+    print("**** Begin Analysis ****\n")
     df = pd.read_csv('sf_long.csv')    
-    print len(df)
-    filtered_df = df[df.Category == 'LARCENY/THEFT'];
-    print len(filtered_df)
     processDF(df)
+
+    filtered_df = df[df.Category == 'LARCENY/THEFT'];
     processDF(filtered_df)
+    print("\n**** End Analysis ****\n")
 
 if __name__ == "__main__":
     main()
